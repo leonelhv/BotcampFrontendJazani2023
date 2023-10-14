91f0c8ac624cdaca904945615c0abda5d59f1c1c
leonelhv <leonel.huachacav@gmail.com>
2023-10-14 12:24:02 -0500

    add react-bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import "@popperjs/core"
+import "bootstrap/dist/js/bootstrap.bundle.min.js"
 
+import "./core/styles/app.scss"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
